f8ec77303fcd77aac80ff39e4d9c2f8c9a9c978b
Alexey <500000vasa100@mail.ru>
2025-09-24 10:46:01 +0300

    kmms_dz1_24_09_25

---
+++ kmms_dz1/io.hpp
@@ -0,0 +1,15 @@
+#ifndef IO_HPP
+#define IO_HPP
+
+#include <iostream>
+#include <vector>
+
+namespace io
+{
+    void printer(const std::string str, std::vector<int> vec, const int n);
+    void inp(const std::string text, int &n);
+
+}
+
+
+#endif
