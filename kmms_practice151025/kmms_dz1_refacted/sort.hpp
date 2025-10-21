8fc6dd0fa0b8715f951483928b79b5a8b6fbd8f8
Yennefer of Vengerberg <63299351+YennOfVengerberg@users.noreply.github.com>
2025-10-21 23:10:00 +0300

    Delete kmms_practice151025 directory

--- kmms_practice151025/kmms_dz1_refacted/sort.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SORTS_HPP
-#define SORTS_HPP
-
-namespace sorts {
-    void merge(int *vec, int left, int mid, int right);
-    void insertionSort(int *vec, const int size);
-    void modifiedMergeSort(int *vec, int left, int right, int threshold, const int size);
-}
-#endif
